e7afa191075cfa391f71576942ef11e4ba20edcb
sknjpn <sknjpn@gmail.com>
2022-10-10 10:19:34 +0900

    add json type msg

--- index.js
+++ index.js
@@ -8,18 +8,29 @@ var server = new ws.Server({ port: 3000 });
 server.on('connection', socket => {
     console.log('connection start')
 
-    socket.send(shortid.generate(), { binary: false });
+    socket.uid = shortid.generate();
+    socket.send(JSON.stringify({ uid: socket.uid, type: 'whoami' }), { binary: false });
 
     socket.on('message', message => {
         console.log('Receive:');
         console.log(message);
 
-        server.clients.forEach(client => {
-            client.send(message, { binary: false });
-        });
+        try {
+            const obj = JSON.parse(message);
+            obj.uid = socket.uid;
+
+            server.clients.forEach(client => {
+                client.send(JSON.stringify(obj), { binary: false });
+            });
+        }
+        catch (e) {
+            console.log(e);
+        }
     });
 
     socket.on('close', () => {
         console.log('connection close');
+
+        server.clients.forEach(client => client.send(JSON.stringify({ uid: socket.uid, type: 'close' }), { binary: false }));
     });
 });
